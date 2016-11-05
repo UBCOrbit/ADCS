ae423347f99dc137af4d03e2816e70bb17c917fc
bhorbit <bhorbit@users.noreply.github.com>
2016-11-05 13:21:49 -0700

    Migrate Sun Sensor Arduino code from Google Drive to Github

---
+++ Sun_Sensor_Arduino_Code/ATTiny_Code/ATTiny_Code.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+// Definitions
+#define rxPin 10
+#define txPin 9
+
+SoftwareSerial mySerial(rxPin, txPin);
+int data1;
+int data2;
+int data3;
+int data4;
+void setup() 
+{
+  mySerial.begin(9600);
+}
+
+void loop()
+{
+  data1 = analogRead(0);
+  data2 = analogRead(1);
+  data3 = analogRead(2);
+  data4 = analogRead(3);
+  mySerial.print("Sensor1 :");
+  mySerial.print(data1, DEC);
+  mySerial.print("\n");
+  mySerial.print("Sensor2 :");
+  mySerial.print(data2, DEC);
+  mySerial.print("\n");
+  mySerial.print("Sensor3 :");
+  mySerial.print(data3, DEC);
+  mySerial.print("\n");
+  mySerial.print("Sensor4 :");
+  mySerial.print(data4, DEC);
+  delay(900);
+  mySerial.print("\n\n");
+  delay(100);
+}
---
+++ Sun_Sensor_Arduino_Code/Arduino_Uno_Code/Arduino_Uno_Code.ino
@@ -0,0 +1,21 @@
+#import <SoftwareSerial.h>
+
+#define RX 2
+#define TX 3
+
+SoftwareSerial USerial(RX, TX);
+int data;
+boolean newLine;
+void setup()
+{
+  USerial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while(USerial.available() > 0){
+    char data = USerial.read();
+    Serial.print(data);
+  } 
+}
